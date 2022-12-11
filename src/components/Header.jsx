c57319b978a0159b5604a60128c189bb5ab3a7da
GJZ26 <adolfogj26@gmail.com>
2022-12-11 14:03:40 -0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import "../assets/stylesheets/components-styles/Header.css"
+import { NavLink } from "react-router-dom";
+import { AiOutlineUser } from 'react-icons/ai'
+
+
+function Header() {
+    return (
+        <nav>
+            <img className="logo-type" src="src/assets/Images/Caduceus.svg" alt="Caduceus" />
+            <div className="nav-links">
+                <NavLink to="/" className="link">Inicio</NavLink>
+                <NavLink to="/services" className="link">Servicios</NavLink>
+                <NavLink to="contact" className="link">Contáctanos</NavLink>
+                <NavLink to="login" className="link"><AiOutlineUser /></NavLink>
+            </div>
+        </nav>
+    );
+}
+
+export default Header;
---
+++ src/views/Landing.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+function Landing() {
+    return (
+        <>
+        <h1>Hola</h1>
+        </>
+    );
+}
+
+export default Landing;
