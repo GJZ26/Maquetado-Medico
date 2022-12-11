715bb5d8beaf5ffbafa29ba4afe27a8b3f654169
GJZ26 <adolfogj26@gmail.com>
2022-12-11 16:40:14 -0600

    finishin landing page

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,9 +1,15 @@
 import Greeting from "../components/Greeting";
+import About from "../components/About";
+import Specialty from "../components/Specialty";
+import Experience from "../components/Experience";
 
 function Landing() {
     return (
         <>
             <Greeting/>
+            <About/>
+            <Specialty/>
+            <Experience/>
         </>
     );
 }
